b76268dde11372b513183d45422e5572584b5070
daovanhai <1851161809@e.tlu.edu.vn>
2021-05-28 21:17:58 +0700

    sửa giao diện đăng nhập

---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+   
+</head>
+<body>
+        <div class="wrapper">
+            <nav class="navbar">
+                <img class="logo" src="{{asset('logo.png')}}">
+                <ul>
+                    <li><a href="{{route('home')}}">Đăng nhập</a></li>
+                </ul>
+            </nav>
+            <div class="center">
+                <h2>Bán sản phẩm</h2>
+                <h1>Đồ điện tử</h1>
+                <div class="buttons">
+                    <button2 class="btn2">Dự Án</button2>
+                    <button2 class="btn2">Nhóm 10</button2>
+                </div>
+            </div>
+        </div>
+
+</body>
+</html>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -18,8 +18,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/home', function () {
     return view('home');
 })->name('home')->middleware('CheckLogin');;
 
